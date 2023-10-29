bdac5b1dc210fefd67fb798019a6ecf70b61897c
Robin Linus <RobinLinus@users.noreply.github.com>
2023-10-30 00:31:55 +0100

    Update blake3.js

--- script-vm/examples/blake3.js
+++ script-vm/examples/blake3.js
@@ -46,80 +46,80 @@ u32_push(0x04030201),
 
 `,
 
-    // Stack:  m1 m0 d c b a  |
+// Stack:  m1 m0 d c b a  |
 
-    // z = a+b+m0 
-    u32_pick(1),
-    u32_zip(0, 1),
-    u32_add,
-    u32_zip(0, 4),
-    u32_add,
-    // Stack:  m1 d c b  |  z
+// z = a+b+m0 
+u32_pick(1),
+u32_zip(0, 1),
+u32_add,
+u32_zip(0, 4),
+u32_add,
+// Stack:  m1 d c b  |  z
 
-    // y = (d^z) >>> 16
-    u32_pick(0),
-    u32_zip(0, 4),
-    u32_xor(6),
-    u32_rrot16,
-    // Stack:  m1 c b  |  z y
+// y = (d^z) >>> 16
+u32_pick(0),
+u32_zip(0, 4),
+u32_xor(6),
+u32_rrot16,
+// Stack:  m1 c b  |  z y
 
-    // x = y+c
-    u32_pick(0),
-    u32_zip(0, 4),
-    u32_add,
-    // Stack:  m1 b  |  z y x
+// x = y+c
+u32_pick(0),
+u32_zip(0, 4),
+u32_add,
+// Stack:  m1 b  |  z y x
 
-    // w = (b^x) >>> 12
-    u32_pick(0),
-    u32_zip(0, 4),
-    u32_xor(6),
-    u32_rrot12,
-    // Stack:  m1  |  z y x w
+// w = (b^x) >>> 12
+u32_pick(0),
+u32_zip(0, 4),
+u32_xor(6),
+u32_rrot12,
+// Stack:  m1  |  z y x w
 
-    // v = z+w+m1
-    u32_pick(0),
-    u32_zip(0, 4),
-    u32_add,
-    u32_zip(0, 4),
-    u32_add,
-    // Stack:  |  y x w v
+// v = z+w+m1
+u32_pick(0),
+u32_zip(0, 4),
+u32_add,
+u32_zip(0, 4),
+u32_add,
+// Stack:  |  y x w v
 
-    // u = (y^v) >>> 8
-    u32_pick(0),
-    u32_zip(0, 4),
-    u32_xor(5),
-    u32_rrot8,
-    // Stack:  |  x w v u
+// u = (y^v) >>> 8
+u32_pick(0),
+u32_zip(0, 4),
+u32_xor(5),
+u32_rrot8,
+// Stack:  |  x w v u
 
-    // t = x+u
-    u32_pick(0),
-    u32_zip(0, 4),
-    u32_add,
-    // Stack:  |  w v u t
+// t = x+u
+u32_pick(0),
+u32_zip(0, 4),
+u32_add,
+// Stack:  |  w v u t
 
-    // s = (w^t) >>> 7
-    u32_pick(0),
-    u32_zip(0, 4),
-    u32_xor(5),
-    u32_rrot7,
-    // Stack:  |  v u t s
+// s = (w^t) >>> 7
+u32_pick(0),
+u32_zip(0, 4),
+u32_xor(5),
+u32_rrot7,
+// Stack:  |  v u t s
 
 
 
-    //
-    // Cleanup our stack to make the result more readable
-    //
-    u32_toaltstack,
-    u32_toaltstack,
-    u32_toaltstack,
-    u32_toaltstack,
+//
+// Cleanup our stack to make the result more readable
+//
+u32_toaltstack,
+u32_toaltstack,
+u32_toaltstack,
+u32_toaltstack,
 
-    u32_drop_xor_table,
+u32_drop_xor_table,
 
-    u32_fromaltstack,
-    u32_fromaltstack,
-    u32_fromaltstack,
-    u32_fromaltstack
+u32_fromaltstack,
+u32_fromaltstack,
+u32_fromaltstack,
+u32_fromaltstack
 
 ].join('')
 
