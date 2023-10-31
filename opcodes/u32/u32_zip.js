35b2cbebc367d5f6900b65e32d548eeb3ede504d
stillsaiko <stillsaiko@gmail.com>
2023-10-31 16:18:14 +0100

    Add `u32_copy_zip` and `u32_zip_copy`

--- opcodes/u32/u32_zip.js
+++ opcodes/u32/u32_zip.js
@@ -23,6 +23,56 @@ OP_ROLL
 `
 }
 
+const u32_copy_zip (a, b) => {
+	if(a >= b) throw 'Error: a >= b'
+	
+	a = (a+1) * 4 - 1
+	b = (b+1) * 4 - 1
+	return `
+<${a}>
+OP_PICK
+<${b+1}>
+OP_ROLL
+<${a+1}>
+OP_PICK
+<${b+2}>
+OP_ROLL
+<${a+2}>
+OP_PICK
+<${b+3}>
+OP_ROLL
+<${a+3}>
+OP_PICK
+<${b+4}>
+OP_ROLL
+`
+}
+
+const u32_zip_copy (a, b) => {
+	if(a >= b) throw 'Error: a >= b'
+	
+	a = (a+1) * 4 - 1
+	b = (b+1) * 4 - 1
+	return `
+<${a}>
+OP_ROLL
+<${b}>
+OP_PICK
+<${a+1}>
+OP_ROLL
+<${b}>
+OP_PICK
+<${a+2}>
+OP_ROLL
+<${b}>
+OP_PICK
+<${a+3}>
+OP_ROLL
+<${b}>
+OP_PICK
+`
+}
+
 const u32_pick = a => {
 	a = (a+1) * 4 - 1
 	return `
