54dd7611d80def3f2e0343d4cea01377c63b6d4b
stillsaiko <stillsaiko@gmail.com>
2023-12-08 20:11:56 +0100

    Add merkle verification to `vm.js`

--- run/vm.js
+++ run/vm.js
@@ -1,18 +1,46 @@
 import '../libs/blake3.js'
 import { blake3 } from '../libs/blake3.js'
 import { toHex, concat } from '../libs/bytes.js'
-import { buildTree } from '../libs/merkle.js'
+import { buildTree, buildPath } from '../libs/merkle.js'
 import { ASM_ADD, ASM_SUB, ASM_MUL, ASM_JMP, ASM_BEQ, ASM_BNE } from '../transactions/bitvm.js'
 import { TRACE_LEN } from '../transactions/bitvm.js'
 
-const traceExecution = (PC, instruction, memory) => {
-    const root = blake3(concat(buildTree(memory.mem.map(x => new Uint32Array([x]).buffer)), new Uint32Array([memory.pc]).buffer))
-    console.log(`PC: ${PC},  Instruction: ${(instruction+'').padEnd(9,' ')} Memory: [${memory.mem}]  State Root: ${toHex(root)}`)
-    return root
+class Memory {
+    pc
+    mem
+    stepCount = 0
+
+    constructor(mem, pc = 0) {
+        this.mem = mem
+        this.pc = pc
+    }
+
+    read(addr) {
+        if(addr < 0) throw `ERROR: address=${addr} is negative`
+        if(addr >= this.mem.length) throw `ERROR: address=${addr} >= memory.length=${this.mem.length}`
+        return this.mem[addr]
+    }
+
+    path(addr) {
+        if(addr < 0) throw `ERROR: address=${addr} is negative`
+        if(addr >= this.mem.length) throw `ERROR: address=${addr} >= memory.length=${this.mem.length}`
+        return [buildPath(this.mem.map(x => new Uint32Array([x]).buffer), addr), new Uint32Array([memory.pc]).buffer]
+    }
+
+    verify(path, pc, value, address) {
+        const root = verifyPath(path, new Uint32Array([value]).buffer, address)
+        return blake3(concat(root, pc)).slice(0, 20).buffer
+    }
+
+    get root() {
+        const root = buildTree(this.mem.map(x => new Uint32Array([x]).buffer))
+        return blake3(concat(root, new Uint32Array([memory.pc]).buffer)).slice(0, 20).buffer
+    }
 }
 
 const executeInstruction = (memory, instruction) => {
-    const root = traceExecution(memory.pc, instruction, memory)
+    // traceExecution
+    console.log(`PC: ${memory.pc},  Instruction: ${(instruction+'').padEnd(9,' ')} Memory: [${memory.mem}]`)
 
     switch (instruction[0]) {
         case ASM_ADD:
@@ -48,50 +76,14 @@ const executeInstruction = (memory, instruction) => {
             memory.pc += 1
             break
     }
-    return [memory, root] 
 }
 
-class Memory {
-    pc
-    mem
-
-    constructor(mem, pc = 0) {
-        this.mem = mem
-        this.pc = pc
-    }
-}
-
-class Trace {
-    #roots
-
-    constructor(roots){
-        this.#roots = roots
-    }
-
-    getRoot(index){
-        // if(index >= this.#roots.length)
-        //     return '0000000000000000000000000000000000000000'
-        index = Math.min(index, this.#roots.length - 1)
-        return toHex(this.#roots[index])
-    }
-
-}
-
-export const runVM = (program, data, maxSteps=TRACE_LEN) => {
-    let memory = new Memory([...data], 0)
-    let root
-    let trace = []
-    let stepCount = 0
-    while (memory.pc >= 0 && memory.pc < program.length && stepCount < maxSteps) {
+export const runVM = (program, memory_entries, maxSteps=TRACE_LEN) => {
+    let memory = new Memory([...memory_entries], 0)
+    while (memory.pc >= 0 && memory.pc < program.length && memory.stepCount < maxSteps) {
         const currentInstruction = program[memory.pc];
-        [memory, root] = executeInstruction(memory, currentInstruction)
-        trace.push(root)
-        stepCount++
+        executeInstruction(memory, currentInstruction)
+        memory.stepCount++
     }
-    return new Trace(trace)
-}
-
-export const readFromMemory = (address) => {
-    // TODO: return value + merkle proof here
-    throw 'Not implemented!'
+    return memory
 }
