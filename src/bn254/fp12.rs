23612861233ce299c41127341d85b367c295bf57
robinlinus <RobinLinus@users.noreply.github.com>
2024-03-28 20:34:39 +0100

    Add fp12 push and equalverify

--- src/bn254/fp12.rs
+++ src/bn254/fp12.rs
@@ -1,4 +1,5 @@
 use crate::bn254::fp6::Fp6;
+use crate::bn254::fp::Fp;
 use crate::treepp::{pushable, script, Script};
 
 pub struct Fp12;
@@ -20,6 +21,14 @@ impl Fp12 {
             { Fp6::double(a + 6) }
         }
     }
+
+    pub fn equalverify() -> Script {
+        script!{
+            for i in 0..12 {
+                { Fp::equalverify(23 - i * 2, 11 - i) }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -27,12 +36,21 @@ mod test {
     use crate::bn254::fp::Fp;
     use crate::bn254::fp12::Fp12;
     use crate::execute_script;
-    use crate::treepp::{pushable, script};
+    use crate::treepp::*;
+    use ark_bn254::Fq12;
     use ark_ff::Field;
     use ark_std::UniformRand;
     use num_bigint::BigUint;
     use rand::SeedableRng;
     use rand_chacha::ChaCha20Rng;
+
+    fn fp12_push(element: Fq12) -> Script {
+        script!{
+            for elem in element.to_base_prime_field_elements() {
+                { Fp::push_u32_le(&BigUint::from(elem).to_u32_digits()) }
+           }
+        }
+    }
 
     #[test]
     fn test_bn254_fp12_add() {
@@ -44,19 +62,11 @@ mod test {
             let c = &a + &b;
 
             let script = script! {
-                for elem in a.to_base_prime_field_elements() {
-                     { Fp::push_u32_le(&BigUint::from(elem).to_u32_digits()) }
-                }
-                for elem in b.to_base_prime_field_elements() {
-                     { Fp::push_u32_le(&BigUint::from(elem).to_u32_digits()) }
-                }
+                { fp12_push(a) }
+                { fp12_push(b) }
                 { Fp12::add(12, 0) }
-                for elem in c.to_base_prime_field_elements() {
-                     { Fp::push_u32_le(&BigUint::from(elem).to_u32_digits()) }
-                }
-                for i in 0..12 {
-                    { Fp::equalverify(23 - i * 2, 11 - i) }
-                }
+                { fp12_push(c) }
+                { Fp12::equalverify() }
                 OP_TRUE
             };
             let exec_result = execute_script(script);
@@ -73,16 +83,10 @@ mod test {
             let c = a.double();
 
             let script = script! {
-                for elem in a.to_base_prime_field_elements() {
-                    { Fp::push_u32_le(&BigUint::from(elem).to_u32_digits()) }
-                }
+                { fp12_push(a) }
                 { Fp12::double(0) }
-                for elem in c.to_base_prime_field_elements() {
-                    { Fp::push_u32_le(&BigUint::from(elem).to_u32_digits()) }
-                }
-                for i in 0..12 {
-                    { Fp::equalverify(23 - i * 2, 11 - i) }
-                }
+                { fp12_push(c) }
+                { Fp12::equalverify() }
                 OP_TRUE
             };
             let exec_result = execute_script(script);
