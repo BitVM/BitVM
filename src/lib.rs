42ea5a3768e204d44dea10eab676fe10460fedc1
Lukas <lucidLuckylee@users.noreply.github.com>
2024-04-02 11:32:15 +0200

    Print stack as hex values

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 // Re-export what is needed to write treepp scripts
 pub mod treepp {
     pub use crate::execute_script;
@@ -7,12 +8,34 @@ pub mod treepp {
     pub use bitcoin::ScriptBuf as Script;
 }
 
-use bitcoin::{hashes::Hash, TapLeafHash, Transaction};
+use core::fmt;
+
+use bitcoin::{hashes::Hash, hex::DisplayHex, TapLeafHash, Transaction};
 use bitcoin_scriptexec::{Exec, ExecCtx, ExecutionResult, Options, TxTemplate};
 
 pub mod bigint;
 pub mod bn254;
 pub mod signatures;
+
+/// A wrapper for the stack types to print them better.
+struct FmtStack<'a>(&'a Vec<Vec<u8>>);
+impl<'a> fmt::Display for FmtStack<'a> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let mut iter = self.0.iter().enumerate().peekable();
+                write!(f, "\n0:\t\t ")?;
+		while let Some((index, item)) = iter.next() {
+			write!(f, "0x{:8}", item.as_hex())?;
+			if iter.peek().is_some() {
+                                if (index + 1) % 4 == 0 {
+                                    write!(f, "\n{}:\t\t", index + 1)?;
+                                }
+				write!(f, " ")?;
+			}
+		}
+		Ok(())
+	}
+}
+
 
 pub fn execute_script(script: bitcoin::ScriptBuf) -> ExecutionResult {
     let mut exec = Exec::new(
@@ -40,17 +63,34 @@ pub fn execute_script(script: bitcoin::ScriptBuf) -> ExecutionResult {
         }
     }
     let res = exec.result().unwrap();
-    // if !res.success {
-    //     println!(
-    //         "Remaining script: {}",
-    //         exec.remaining_script().to_asm_string()
-    //     );
-    //     // TODO: Print stack with hex values
-    //     println!("Remaining stack: {:?}", exec.stack());
-    //     println!("Last Opcode: {:?}", res.opcode);
-    //     println!("StackSize: {:?}", exec.stack().len());
-    //     println!("{:?}", res.clone().error.map(|e| format!("{:?}", e)));
-    // }
+    //if !res.success {
+    //    println!(
+    //        "Remaining script: {}",
+    //        exec.remaining_script().to_asm_string()
+    //    );
+    //
+    //    println!("Remaining stack: {}", FmtStack(exec.stack()));
+    //    println!("Last Opcode: {:?}", res.opcode);
+    //    println!("StackSize: {:?}", exec.stack().len());
+    //    println!("{:?}", res.clone().error.map(|e| format!("{:?}", e)));
+    //}
 
     res.clone()
+}
+
+#[cfg(test)]
+mod test {
+    use super::treepp::*;
+
+    #[test]
+    fn test_script_debug() {
+            let script = script! {
+                OP_TRUE
+                DEBUG
+                OP_TRUE
+                OP_VERIFY
+            };
+            let exec_result = execute_script(script);
+            assert!(!exec_result.success);
+    }
 }
