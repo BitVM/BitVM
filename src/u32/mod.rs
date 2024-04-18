c69d9ef9d8334ea0af27ded8f498e071589b7ef5
AndreW0ng <zhuowangy2k@outlook.com>
2024-04-18 16:28:25 +0800

    Add sha256 (#54)
    * copy and or from bitvm v1
    
    * init sha256
    
    * add transform data in sha256
    
    * reorg output bytes from little endian to big endian
    
    * implement sha256
    
    * update push_reverse_bytes_to_alt to enable hash
    
    * add example for sha256 twice to block hash
    
    * add some comments and remove unused codes
    
    ---------
    
    Co-authored-by: Xavier-96 <heu13045189326@outlook.com>

--- src/u32/mod.rs
+++ src/u32/mod.rs
@@ -1,4 +1,6 @@
 pub mod u32_add;
+pub mod u32_and;
+pub mod u32_or;
 pub mod u32_rrot;
 pub mod u32_std;
 pub mod u32_xor;
---
+++ src/u32/u32_or.rs
@@ -0,0 +1,166 @@
+#![allow(dead_code)]
+
+use crate::treepp::{pushable, script, Script};
+use crate::u32::u32_zip::u32_copy_zip;
+
+/// Bitwise OR of two u8 elements.
+/// Expects the u8_xor_table to be on the stack.
+///
+pub fn u8_or(i: u32) -> Script {
+    script! {
+        // f_A = f(A)
+        OP_DUP
+        {i}
+        OP_ADD
+        OP_PICK
+
+        // A_even = f_A << 1
+        OP_DUP
+        OP_DUP
+        OP_ADD
+
+        // A_odd = A - A_even
+        OP_ROT
+        OP_SWAP
+        OP_SUB
+
+        // f_B = f(B)
+        OP_ROT
+        OP_DUP
+        {i + 1}
+        OP_ADD
+        OP_PICK
+
+        // B_even = f_B << 1
+        OP_DUP
+        OP_DUP
+        OP_ADD
+
+        // B_odd = B - B_even
+        OP_ROT
+        OP_SWAP
+        OP_SUB
+
+        // A_andxor_B_even = f_A + f_B
+        OP_SWAP
+        3
+        OP_ROLL
+        OP_ADD
+
+        // A_or_B_even = f(A_andxor_B_even << 1) + f(A_andxor_B_even)
+        OP_DUP
+        OP_DUP
+        OP_ADD
+        OP_DUP         // The left shift may overflow 1 bit
+        255
+        OP_GREATERTHAN
+        OP_IF
+            256
+            OP_SUB
+        OP_ENDIF
+        {i + 1}
+        OP_ADD
+        OP_PICK
+        OP_SWAP
+        {i + 1}
+        OP_ADD
+        OP_PICK
+        OP_ADD
+
+        // A_andxor_B_odd = A_odd + B_odd
+        OP_SWAP
+        OP_ROT
+        OP_ADD
+
+        // A_or_B_odd = f(A_andxor_B_odd << 1) + f(A_andxor_B_odd)
+        OP_DUP
+        OP_DUP
+        OP_ADD
+        OP_DUP
+        255
+        OP_GREATERTHAN
+        OP_IF
+            256
+            OP_SUB
+        OP_ENDIF
+        {i}
+        OP_ADD
+        OP_PICK
+        OP_SWAP
+        {i}
+        OP_ADD
+        OP_PICK
+        OP_ADD
+
+        // A_or_B = A_or_B_odd + (A_or_B_even << 1)
+        OP_SWAP
+        OP_DUP
+        OP_ADD
+        OP_ADD
+    }
+}
+
+/// Bitwise OR of two u32 elements.
+/// Expects the u8_xor_table to be on the stack
+pub fn u32_or(a: u32, b: u32, stack_size: u32) -> Script {
+    assert_ne!(a, b);
+
+    script! {
+        {u32_copy_zip(a, b)}
+
+        {u8_or(8 + (stack_size - 2) * 4)}
+
+        OP_TOALTSTACK
+
+        {u8_or(6 + (stack_size - 2) * 4)}
+
+        OP_TOALTSTACK
+
+        {u8_or(4 + (stack_size - 2) * 4)}
+
+        OP_TOALTSTACK
+
+        {u8_or(2 + (stack_size - 2) * 4)}
+
+
+        OP_FROMALTSTACK
+        OP_FROMALTSTACK
+        OP_FROMALTSTACK
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::treepp::pushable;
+    use crate::treepp::{execute_script, script};
+    use crate::u32::u32_or::*;
+    use crate::u32::u32_std::*;
+    use crate::u32::u32_xor::{u8_drop_xor_table, u8_push_xor_table};
+    use rand::Rng;
+
+    fn or(x: u32, y: u32) -> u32 { x | y }
+
+    #[test]
+    fn test_or() {
+        for _ in 0..100 {
+            let mut rng = rand::thread_rng();
+            let x: u32 = rng.gen();
+            let y: u32 = rng.gen();
+            let exec_script = script! {
+                {u8_push_xor_table()}
+                {u32_push(x)}
+                {u32_push(y)}
+                {u32_or(0, 1, 3)}
+                {u32_push(or(x, y))}
+                {u32_equal()}
+                OP_TOALTSTACK
+                {u32_drop()} // drop y
+                {u8_drop_xor_table()}
+                OP_FROMALTSTACK
+            };
+            let res = execute_script(exec_script);
+            assert_eq!(res.success, true);
+        }
+    }
+}
