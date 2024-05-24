77e864868c3346ea279030dc96568c3893f30548
Paul Cheng <chengyuejia@foxmail.com>
2024-05-24 14:31:02 +0800

    chore: remove redunction mod

--- src/groth16/mod.rs
+++ src/groth16/mod.rs
@@ -1,3 +1,2 @@
 pub mod checkpairing_with_c_wi_groth16;
-pub mod pairing;
 pub mod verifier;
--- src/groth16/verifier.rs
+++ src/groth16/verifier.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use ark_bn254::{Bn254, G1Affine, G1Projective, G2Affine};
 use ark_ec::{
-    bn::{g2::mul_by_char, Bn, BnConfig, G2Prepared},
+    bn::{Bn, BnConfig, G2Prepared},
     pairing::Pairing,
     short_weierstrass::{Projective, SWCurveConfig},
     AffineRepr, CurveGroup, VariableBaseMSM,
@@ -144,30 +144,9 @@ impl Verifier {
             f * wi * (c_inv.pow((exp).to_u64_digits()).inverse().unwrap())
         };
 
-        assert_eq!(hint, c.pow(p_pow3.to_u64_digits()));
+        assert_eq!(hint, c.pow(p_pow3.to_u64_digits()), "hint isn't correct!");
 
         println!("hint is correct!\n\n");
-
-        // calculate offchain
-        let eval_points = vec![sum_ai_abc_gamma.into(), proof.c.into(), vk.alpha_g1.into()];
-        // let p4 = <G1Affine as Into<<Bn254 as Pairing>::G1Prepared>>::into(proof.a);
-        let line_beta: G2Prepared<ark_bn254::Config> = (-vk.beta_g2).into();
-        let lines = vec![
-            pvk.gamma_g2_neg_pc.to_owned(),
-            pvk.delta_g2_neg_pc.to_owned(),
-            line_beta,
-        ];
-        let expect_f = Self::quad_miller_loop_with_c_wi_rust(
-            eval_points,
-            proof.a,
-            proof.b.into(),
-            &lines,
-            c,
-            c_inv,
-            wi,
-        );
-
-        println!("expect_res: {}", expect_f);
 
         let quad_miller_loop_with_c_wi = Pairing2::quad_miller_loop_with_c_wi(&q_prepared);
 
@@ -225,146 +204,6 @@ impl Verifier {
             { Fq12::equalverify() }
             OP_TRUE
         }
-    }
-
-    pub fn quad_miller_loop_with_c_wi_rust(
-        eval_points: Vec<G1Affine>,
-        P4: G1Affine,
-        Q4: G2Affine,
-        // lines: &[Vec<(Fq2, Fq2)>],
-        lines: &Vec<G2Prepared<ark_bn254::Config>>,
-        c: ark_bn254::Fq12,
-        c_inv: ark_bn254::Fq12,
-        wi: ark_bn254::Fq12,
-        // TODO: What's B in stack
-    ) -> ark_bn254::Fq12 {
-        assert_eq!(eval_points.len(), 3, "Should contains 4 G1Affine: P1,P2,P3");
-        assert_eq!(lines.len(), 3, "Only precompute lines for Q1,Q2,Q3");
-        assert_eq!(c * c_inv, ark_bn254::Fq12::ONE, "Check if c·c^−1 = 1");
-
-        // let P4 = eval_points[3].clone();
-        // let Q4_projective: G2Projective = Q4.into_group();
-        let mut T4 = ark_ec::bn::g2::G2HomProjective::<ark_bn254::Config> {
-            x: Q4.x,
-            y: Q4.y,
-            z: ark_bn254::Fq2::one(),
-        };
-
-        // constants
-        let two_inv = ark_bn254::Fq::one().double().inverse().unwrap();
-
-        // 1. f = c_inv
-        let mut f = c_inv;
-        println!("1.f: {:?}", f.to_string());
-
-        let mut lines_iters = lines
-            .iter()
-            .map(|item| item.ell_coeffs.iter())
-            .collect::<Vec<_>>();
-
-        // 2. miller loop part, 6x + 2
-        for i in (1..ark_bn254::Config::ATE_LOOP_COUNT.len()).rev() {
-            // for i in 4..9 {
-            let bit = ark_bn254::Config::ATE_LOOP_COUNT[i - 1];
-
-            // 2.1 double: f = f * f
-            f = f.square();
-
-            // 2.2 mul c
-            //  f = f * c_inv, if digit == 1
-            //  f = f * c, if digit == -1
-            f = if 1 == bit {
-                f * c_inv
-            } else if bit == -1 {
-                f * c
-            } else if bit == 0 {
-                f
-            } else {
-                panic!("bit is not in (-1,1), bit={bit}");
-            };
-
-            // 2.3 accumulate double lines (fixed and non-fixed)
-            // 2.3.1(fixed) f = f * double_line_Q(P). fixed points: P1, P2, P3
-            for (line_i, pi) in lines_iters.iter_mut().zip(eval_points.iter()) {
-                let line_i_0 = line_i.next().unwrap();
-                Bn254::ell(&mut f, line_i_0, pi);
-            }
-
-            // 2.3.2(non-fixed) double line with T4 (projective coordinates)
-            let double_line = T4.double_in_place(&two_inv); // TODO: check if the param is 1/2
-
-            // 2.3.3(non-fixed) evaluation double_line. non-fixed points: P4
-            Bn254::ell(&mut f, &double_line, &P4);
-
-            if bit == 1 || bit == -1 {
-                // 2.4 accumulate add lines (fixed and non-fixed)
-                // 2.4.1(fixed) f = f * add_line_eval. fixed points: P1, P2, P3
-                for (line_i, pi) in lines_iters.iter_mut().zip(eval_points.iter()) {
-                    let line_i_1 = line_i.next().unwrap();
-                    Bn254::ell(&mut f, line_i_1, pi);
-                }
-                // 2.4.2(non-fixed) double line with T4 (projective coordinates)
-                let add_line = if bit == 1 {
-                    T4.add_in_place(&Q4)
-                } else {
-                    // }else if bit == -1 {
-                    let mut neg_q4 = Q4.clone();
-                    neg_q4.y.neg_in_place();
-                    T4.add_in_place(&neg_q4)
-                };
-
-                // 2.4.3(non-fixed) evaluation double_line. non-fixed points: P4
-                Bn254::ell(&mut f, &add_line, &P4);
-            }
-        }
-        println!("2.f: {:?}", f.to_string());
-
-        // 3. f = f * c_inv^p * c^{p^2}
-        let MODULUS_STR: &str = "30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47";
-        let MODULUS: BigUint = BigUint::from_str_radix(MODULUS_STR, 16).unwrap();
-        f = f * c_inv.pow(MODULUS.to_u64_digits()) * c.pow(MODULUS.pow(2).to_u64_digits());
-        println!("3.f: {:?}", f.to_string());
-
-        // // 4. f = f * wi . scale f
-        // f = f * wi;
-        // println!("4.f: {:?}", f.to_string());
-
-        // // 5 add lines (fixed and non-fixed)
-        // // 5.1(fixed) f = f * add_line_eval. fixed points: P1, P2, P3
-        // for (line_i, pi) in lines_iters.iter_mut().zip(eval_points.iter()) {
-        //     let line_i_1 = line_i.next().unwrap();
-        //     Bn254::ell(&mut f, line_i_1, pi);
-        // }
-        // // 5.2 one-time frobenius map to compute phi_Q
-        // //     compute phi(Q) with Q4
-        // let phi_Q = mul_by_char::<ark_bn254::Config>(Q4.clone());
-
-        // let add_line = T4.add_in_place(&phi_Q);
-
-        // // 5.4(non-fixed) evaluation add_lin. non-fixed points: P4
-        // Bn254::ell(&mut f, &add_line, &P4);
-        // println!("5.f: {:?}", f.to_string());
-
-        // // 6. add lines (fixed and non-fixed)
-        // // 6.1(fixed) f = f * add_line_eval. fixed points: P1, P2, P3
-        // for (line_i, pi) in lines_iters.iter_mut().zip(eval_points.iter()) {
-        //     // TODO: where is f?? and where is double line?
-        //     let line_i_1 = line_i.next().unwrap();
-        //     Bn254::ell(&mut f, line_i_1, pi);
-        // }
-        // // 6.2 two-time frobenius map to compute phi_Q
-        // //     compute phi_Q_2 with phi_Q
-        // // mul_by_char: used to q's frob...map.
-        // let mut phi_Q_2 = mul_by_char::<ark_bn254::Config>(phi_Q.clone());
-        // phi_Q_2.y.neg_in_place();
-        // let add_line = T4.add_in_place(&phi_Q_2);
-        // println!("6.2.f: {:?}", f.to_string());
-
-        // // 6.3(non-fixed) evaluation add_lin. non-fixed points: P4
-        // Bn254::ell(&mut f, &add_line, &P4);
-        // println!("6.3.f: {:?}", f.to_string());
-
-        f
     }
 }
 
