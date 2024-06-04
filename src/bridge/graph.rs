d8fb95393729ee712a7a493ecc9c1af3e71a25d2
aristotleee <148852099+aristotleee@users.noreply.github.com>
2024-06-04 20:20:50 +0800

    remove invalid secret and use existing unspendable pubkey

--- src/bridge/graph.rs
+++ src/bridge/graph.rs
@@ -12,7 +12,7 @@ pub const FEE_AMOUNT: u64 = 1_000;
 pub const DUST_AMOUNT: u64 = 10_000;
 
 lazy_static! {
-    static ref UNSPENDABLE_PUBKEY: XOnlyPublicKey = XOnlyPublicKey::from_str(
+    pub static ref UNSPENDABLE_PUBKEY: XOnlyPublicKey = XOnlyPublicKey::from_str(
         "50929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0"
     )
     .unwrap();
@@ -22,7 +22,6 @@ lazy_static! {
 pub const OPERATOR_SECRET: &str =
     "d898098e09898a0980989b980809809809f09809884324874302975287524398";
 pub const N_OF_N_SECRET: &str = "a9bd8b8ade888ed12301b21318a3a73429232343587049870132987481723497";
-pub const UNSPENDABLE_SECRET: &str = "8ylb10hs645nwc04f47cnnlggvh0np1yi9kp2fk40k3aq96k1kc1n1h09n0ag68e"; // TODO: don't use known secret for unspendable key
 
 pub type CompiledBitVMGraph = HashMap<OutPoint, Vec<Box<dyn BridgeTransaction + 'static>>>;
 
@@ -64,11 +63,10 @@ mod tests {
         let secp = Secp256k1::new();
         let n_of_n_key = Keypair::from_seckey_str(&secp, N_OF_N_SECRET).unwrap();
         let operator_key = Keypair::from_seckey_str(&secp, OPERATOR_SECRET).unwrap();
-        let unspendable_pubkey = Keypair::from_seckey_str(&secp, UNSPENDABLE_SECRET).unwrap();
         let mut context = BridgeContext::new();
         context.set_operator_key(operator_key);
         context.set_n_of_n_pubkey(n_of_n_key.x_only_public_key().0);
-        context.set_unspendable_pubkey(unspendable_pubkey.x_only_public_key().0);
+        context.set_unspendable_pubkey(*UNSPENDABLE_PUBKEY);
 
         let mut client = BitVMClient::new();
         let funding_utxo = client
--- src/bridge/transactions.rs
+++ src/bridge/transactions.rs
@@ -8,7 +8,7 @@ mod tests {
     };
 
     use crate::bridge::{
-        client::BitVMClient, components::{bridge::BridgeTransaction, connector_c::{connector_c_address, connector_c_pre_sign_address}, disprove::DisproveTransaction}, context::BridgeContext, graph::{DUST_AMOUNT, INITIAL_AMOUNT, N_OF_N_SECRET, OPERATOR_SECRET, UNSPENDABLE_SECRET}
+        client::BitVMClient, components::{bridge::BridgeTransaction, connector_c::{connector_c_address, connector_c_pre_sign_address}, disprove::DisproveTransaction}, context::BridgeContext, graph::{DUST_AMOUNT, INITIAL_AMOUNT, N_OF_N_SECRET, OPERATOR_SECRET, UNSPENDABLE_PUBKEY}
     };
 
     use bitcoin::consensus::encode::serialize_hex;
@@ -18,7 +18,6 @@ mod tests {
         let secp = Secp256k1::new();
         let operator_key = Keypair::from_seckey_str(&secp, OPERATOR_SECRET).unwrap();
         let n_of_n_key = Keypair::from_seckey_str(&secp, N_OF_N_SECRET).unwrap();
-        let unspendable_pubkey = Keypair::from_seckey_str(&secp, UNSPENDABLE_SECRET).unwrap();
         let client = BitVMClient::new();
         let funding_utxo_1 = client
             .get_initial_utxo(
@@ -66,7 +65,7 @@ mod tests {
         let mut context = BridgeContext::new();
         context.set_operator_key(operator_key);
         context.set_n_of_n_pubkey(n_of_n_key.x_only_public_key().0);
-        context.set_unspendable_pubkey(unspendable_pubkey.x_only_public_key().0);
+        context.set_unspendable_pubkey(*UNSPENDABLE_PUBKEY);
 
         let mut disprove_tx = DisproveTransaction::new(
             &context,
