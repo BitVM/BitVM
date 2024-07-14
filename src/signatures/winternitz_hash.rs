2025f98b1252729a54dd4333b9a59266491e1c0b
robinlinus <RobinLinus@users.noreply.github.com>
2024-07-14 23:57:56 +0200

    Add separate function for `sign_hash`

--- src/signatures/winternitz_hash.rs
+++ src/signatures/winternitz_hash.rs
@@ -1,6 +1,7 @@
 use crate::treepp::*;
 use crate::signatures::winternitz::{sign, checksig_verify};
 use crate::hash::blake3::blake3_160_var_length;
+use blake3::hash;
 
 const MESSAGE_HASH_LEN: u8 = 20;
 
@@ -16,7 +17,7 @@ pub fn check_hash_sig(sec_key: &str, input_len: usize) -> Script {
         }
 
         // 2. Hash the inputs
-        { blake3_160_var_length( input_len ) }
+        { blake3_160_var_length(input_len) }
 
         // 3. Compare signed message to the hash
         for _ in 0..MESSAGE_HASH_LEN / 4 {
@@ -30,23 +31,27 @@ pub fn check_hash_sig(sec_key: &str, input_len: usize) -> Script {
     }
 }
 
-
+/// Create a Winternitz signature for the blake3 hash of a given message
+pub fn sign_hash(sec_key: &str, message: &[u8]) -> Script {
+    let message_hash = hash(message);
+    let message_hash_bytes = &message_hash.as_bytes()[0..20];
+    script! {
+        { sign(sec_key, message_hash_bytes) }
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use blake3::hash;
 
     #[test]
     fn test_check_hash_sig() {
 
+        // My secret key 
         let my_sec_key = "b138982ce17ac813d505b5b40b665d404e9528e7";
         
         // The message to sign
         let message = *b"This is an arbitrary length input intended for testing purposes....";
-        let message_hash = hash(&message);
-        let message_hash_bytes = &message_hash.as_bytes()[0..20];
-        
 
         let script = script! {
             //
@@ -58,7 +63,7 @@ mod test {
                 { *byte }
             }
             // 2. Push the signature
-            { sign(my_sec_key, message_hash_bytes) }
+            { sign_hash(my_sec_key, &message) }
             
             
             //
