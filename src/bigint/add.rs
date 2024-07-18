8d23746a6b96491faa47f20c5a357d50a3462505
fatih <fatihturkiyed@gmail.com>
2024-07-18 12:00:33 +0300

    bigint optimizations

--- src/bigint/add.rs
+++ src/bigint/add.rs
@@ -4,77 +4,144 @@ use crate::treepp::*;
 impl<const N_BITS: u32, const LIMB_SIZE: u32> BigIntImpl<N_BITS, LIMB_SIZE> {
     /// Double a BigInt
     pub fn double(a: u32) -> Script {
-        script! {
-            { Self::dup_zip(a) }
-
-            { 1 << LIMB_SIZE }
-
-            // A0 + B0
-            limb_add_carry OP_TOALTSTACK
-
-            // from     A1      + B1        + carry_0
-            //   to     A{N-2}  + B{N-2}    + carry_{N-3}
-            for _ in 0..Self::N_LIMBS - 2 {
-                OP_ROT
+        if a == 0 {
+            script! {
+                OP_DUP
                 OP_ADD
+                { 1 << LIMB_SIZE }
                 OP_SWAP
-                limb_add_carry OP_TOALTSTACK
+
+                limb_add_carry2
+                OP_TOALTSTACK
+
+                // from     A1      + B1        + carry_0
+                //   to     A{N-2}  + B{N-2}    + carry_{N-3}
+                for _ in 0..Self::N_LIMBS - 2 {
+                    OP_ROT
+                    OP_DUP
+                    OP_ADD
+                    limb_add_carry3 OP_TOALTSTACK
+                }
+
+                // A{N-1} + B{N-1} + carry_{N-2}
+                OP_NIP
+                OP_SWAP
+                OP_DUP
+                OP_ADD
+                { limb_add_nocarry(Self::HEAD_OFFSET) }
+
+                for _ in 0..Self::N_LIMBS - 1 {
+                    OP_FROMALTSTACK
+                }
             }
+        } else {
+            script! {
+                { Self::dup_zip(a) }
 
-            // A{N-1} + B{N-1} + carry_{N-2}
-            OP_NIP
-            OP_ADD
-            { limb_add_nocarry(Self::HEAD_OFFSET) }
+                OP_ADD
+                { 1 << LIMB_SIZE }
+                OP_SWAP
 
-            for _ in 0..Self::N_LIMBS - 1 {
-                OP_FROMALTSTACK
+                limb_add_carry2
+                OP_TOALTSTACK
+
+                // from     A1      + B1        + carry_0
+                //   to     A{N-2}  + B{N-2}    + carry_{N-3}
+                for _ in 0..Self::N_LIMBS - 2 {
+                    OP_2SWAP
+                    OP_ADD
+                    limb_add_carry3 OP_TOALTSTACK
+                }
+
+                // A{N-1} + B{N-1} + carry_{N-2}
+                OP_NIP
+                OP_ADD
+                { limb_add_nocarry(Self::HEAD_OFFSET) }
+
+                for _ in 0..Self::N_LIMBS - 1 {
+                    OP_FROMALTSTACK
+                }
             }
         }
     }
 
     /// Compute the sum of two BigInts
     pub fn add(a: u32, b: u32) -> Script {
-        script! {
-            { Self::zip(a, b) }
-
-            { 1 << LIMB_SIZE }
-
-            // A0 + B0
-            limb_add_carry OP_TOALTSTACK
-
-            // from     A1      + B1        + carry_0
-            //   to     A{N-2}  + B{N-2}    + carry_{N-3}
-            for _ in 0..Self::N_LIMBS - 2 {
-                OP_ROT
+        if b == 0 {
+            script! {
+                {a * Self::N_LIMBS} OP_ROLL
                 OP_ADD
+    
+                { 1 << LIMB_SIZE }
                 OP_SWAP
-                limb_add_carry OP_TOALTSTACK
+    
+                limb_add_carry2
+                OP_TOALTSTACK
+    
+                // from     A1      + B1        + carry_0
+                //   to     A{N-2}  + B{N-2}    + carry_{N-3}
+                for i in 0..Self::N_LIMBS - 2 {
+                    OP_ROT
+                    {a * Self::N_LIMBS - i + 1} OP_ROLL
+                    OP_ADD
+                    limb_add_carry3 OP_TOALTSTACK
+                }
+    
+                // A{N-1} + B{N-1} + carry_{N-2}
+                OP_NIP
+                {(a-1) * Self::N_LIMBS + 2} OP_ROLL
+                OP_ADD
+                { limb_add_nocarry(Self::HEAD_OFFSET) }
+    
+                for _ in 0..Self::N_LIMBS - 1 {
+                    OP_FROMALTSTACK
+                }
             }
+        } else {
+            script! {
+                { Self::zip(a, b) }
 
-            // A{N-1} + B{N-1} + carry_{N-2}
-            OP_NIP
-            OP_ADD
-            { limb_add_nocarry(Self::HEAD_OFFSET) }
+                OP_ADD
+                { 1 << LIMB_SIZE }
+                OP_SWAP
 
-            for _ in 0..Self::N_LIMBS - 1 {
-                OP_FROMALTSTACK
+                limb_add_carry2
+                OP_TOALTSTACK
+
+                // from     A1      + B1        + carry_0
+                //   to     A{N-2}  + B{N-2}    + carry_{N-3}
+                for _ in 0..Self::N_LIMBS - 2 {
+                    OP_2SWAP
+                    OP_ADD
+                    limb_add_carry3 OP_TOALTSTACK
+                }
+
+                // A{N-1} + B{N-1} + carry_{N-2}
+                OP_NIP
+                OP_ADD
+                { limb_add_nocarry(Self::HEAD_OFFSET) }
+
+                for _ in 0..Self::N_LIMBS - 1 {
+                    OP_FROMALTSTACK
+                }
             }
         }
     }
 
     pub fn add1() -> Script {
         script! {
-            1
+            OP_1ADD
             { 1 << LIMB_SIZE }
+            OP_SWAP
 
-            // A0 + 1
-            limb_add_carry OP_TOALTSTACK
+            limb_add_carry2
+            OP_TOALTSTACK
 
             // from     A1        + carry_0
             //   to     A{N-2}    + carry_{N-3}
             for _ in 0..Self::N_LIMBS - 2 {
-                OP_SWAP
-                limb_add_carry OP_TOALTSTACK
+                OP_ROT
+                limb_add_carry3 OP_TOALTSTACK
             }
 
             // A{N-1} + carry_{N-2}
@@ -103,6 +170,17 @@ pub fn limb_add_carry() -> Script {
     }
 }
 
+
+pub fn limb_add_carry2() -> Script {
+    script! {
+        OP_2DUP
+        OP_LESSTHANOREQUAL
+        OP_TUCK
+        OP_IF
+            2 OP_PICK OP_SUB
+        OP_ENDIF
+    }
+}
 
 pub fn limb_add_carry3() -> Script {
     script! {
@@ -142,7 +220,10 @@ mod test {
 
     #[test]
     fn test_add() {
-        println!("U254.add: {} bytes", U254::add(1, 0).len());
+        println!("U254.add(1, 0): {} bytes", U254::add(1, 0).len());
+        println!("U254.add(2, 0): {} bytes", U254::add(2, 0).len());
+        println!("U254.add(2, 1): {} bytes", U254::add(2, 1).len());
+
         let mut prng = ChaCha20Rng::seed_from_u64(0);
         for _ in 0..100 {
             let a: BigUint = prng.sample(RandomBits::new(254));
@@ -155,6 +236,32 @@ mod test {
                 { U254::add(1, 0) }
                 { U254::push_u32_le(&c.to_u32_digits()) }
                 { U254::equalverify(1, 0) }
+                OP_TRUE
+            };
+            let exec_result = execute_script(script);
+            assert!(exec_result.success);
+
+            let script = script! {
+                { U254::push_u32_le(&a.to_u32_digits()) }
+                { U254::push_u32_le(&b.to_u32_digits()) }
+                { U254::push_u32_le(&b.to_u32_digits()) }
+                { U254::add(2, 0) }
+                { U254::push_u32_le(&c.to_u32_digits()) }
+                { U254::equalverify(1, 0) }
+                { U254::drop() }
+                OP_TRUE
+            };
+            let exec_result = execute_script(script);
+            assert!(exec_result.success);
+
+            let script = script! {
+                { U254::push_u32_le(&a.to_u32_digits()) }
+                { U254::push_u32_le(&b.to_u32_digits()) }
+                { U254::push_u32_le(&b.to_u32_digits()) }
+                { U254::add(2, 1) }
+                { U254::push_u32_le(&c.to_u32_digits()) }
+                { U254::equalverify(1, 0) }
+                { U254::drop() }
                 OP_TRUE
             };
             let exec_result = execute_script(script);
@@ -181,7 +288,8 @@ mod test {
 
     #[test]
     fn test_double() {
-        println!("U254.double: {} bytes", U254::double(0).len());
+        println!("U254.double(0): {} bytes", U254::double(0).len());
+        println!("U254.double(1): {} bytes", U254::double(1).len());
         let mut prng = ChaCha20Rng::seed_from_u64(0);
         for _ in 0..100 {
             let a: BigUint = prng.sample(RandomBits::new(254));
@@ -192,6 +300,18 @@ mod test {
                 { U254::double(0) }
                 { U254::push_u32_le(&c.to_u32_digits()) }
                 { U254::equalverify(1, 0) }
+                OP_TRUE
+            };
+            let exec_result = execute_script(script);
+            assert!(exec_result.success);
+
+            let script = script! {
+                { U254::push_u32_le(&a.to_u32_digits()) }
+                { U254::push_zero() }
+                { U254::double(1) }
+                { U254::push_u32_le(&c.to_u32_digits()) }
+                { U254::equalverify(1, 0) }
+                { U254::drop() }
                 OP_TRUE
             };
             let exec_result = execute_script(script);
