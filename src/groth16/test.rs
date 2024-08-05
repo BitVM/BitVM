4e03eee0ca155aa7024948dd8870e1bf4163b7a0
Lukas <lucidLuckylee@users.noreply.github.com>
2024-08-05 17:48:25 +0200

    Add some debug info in tests

--- src/groth16/test.rs
+++ src/groth16/test.rs
@@ -107,8 +107,10 @@ fn test_groth16_verifier_as_chunks() {
 
     println!("groth16::test_verify_proof = {} bytes", script.len());
 
+    let interval = script.max_op_if_interval();
+    println!("Max if interval: {:?} difference: {}, debug info: {}, {}", interval, interval.1 - interval.0, script.debug_info(interval.0), script.debug_info(interval.1));
     let start = start_timer!(|| "execute_script");
-    let exec_result = execute_script_as_chunks(script, 3_000_000, 100_000);
+    let exec_result = execute_script_as_chunks(script, 4_000_000, 1_000_000);
     end_timer!(start);
 
     assert!(exec_result.success);
