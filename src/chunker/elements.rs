e6fc91ac4768702b3cb55afee097ef7c9261e99c
Andrew0ng <Andrew0ng@bitlayer.ltd>
2024-10-27 23:02:09 +0700

    implement various values using marco

--- src/chunker/elements.rs
+++ src/chunker/elements.rs
@@ -1,7 +1,8 @@
 use std::any::Any;
 
 use super::common::*;
-use crate::bn254::utils::fq6_push_not_montgomery;
+use crate::bn254::curves::{G1Affine, G2Affine};
+use crate::bn254::utils::{fq12_push_not_montgomery, fq6_push_not_montgomery};
 use crate::treepp::*;
 use crate::{chunker::assigner::BCAssigner, execute_script_with_inputs};
 
@@ -20,23 +21,11 @@ impl FqElement {
     }
 }
 
-// (x: Fq, y: Fq)
-#[derive(Clone, Debug)]
-pub struct G1PointType(FqElement);
-// (x: Fq2, y: Fq2)
-#[derive(Clone, Debug)]
-pub struct G2PointType(FqElement);
-// (Fq6)
-#[derive(Clone, Debug)]
-pub struct Fq6Type(FqElement);
-// (Fq12)
-#[derive(Clone, Debug)]
-pub struct Fq12Type(FqElement);
-
 /// data type
 pub enum DataType {
     Fq6Data(ark_bn254::Fq6),
     Fq12Data(ark_bn254::Fq12),
+    G1PointData(ark_bn254::G1Affine),
 }
 
 /// This trait defines the intermediate values
@@ -60,92 +49,104 @@ pub trait ElementTrait {
     fn id(&self) -> &str;
 }
 
-impl Fq6Type {
-    /// Create a new element by using bitcommitment assigner
-    pub fn new<F: BCAssigner>(assigner: &mut F, id: &str) -> Self {
-        assigner.create_hash(id);
-        Self {
-            0: FqElement {
-                identity: id.to_owned(),
-                size: 6,
-                witness_data: None,
-            },
+macro_rules! impl_element_trait {
+    ($element_type:ident, $data_type:ident, $size:expr, $push_method:expr) => {
+        #[derive(Clone, Debug)]
+        pub struct $element_type(FqElement);
+
+        impl $element_type {
+            /// Create a new element by using bitcommitment assigner
+            pub fn new<F: BCAssigner>(assigner: &mut F, id: &str) -> Self {
+                assigner.create_hash(id);
+                Self {
+                    0: FqElement {
+                        identity: id.to_owned(),
+                        size: $size,
+                        witness_data: None,
+                    },
+                }
+            }
         }
-    }
+
+        /// impl element for Fq6
+        impl ElementTrait for $element_type {
+            fn fill_with_data(&mut self, x: DataType) {
+                // TODO: need to be optimized and verify
+
+                match x {
+                    DataType::$data_type(fq6_data) => {
+                        let res = execute_script(script! {
+                            {$push_method(fq6_data)}
+                        });
+                        let witness = extract_witness_from_stack(res);
+                        assert_eq!(witness.len(), self.0.witness_size());
+
+                        self.0.witness_data = Some(witness);
+                    }
+                    _ => panic!("fill wrong data {:?}", x.type_id()),
+                }
+            }
+
+            fn to_witness(&self) -> Option<Witness> {
+                self.0.witness_data.clone()
+            }
+
+            fn from_witness(&self) -> Option<DataType> {
+                // TODO:
+                todo!()
+            }
+
+            fn to_hash(&self) -> Option<BLAKE3HASH> {
+                // TODO: need to be optimized and verify
+                match self.0.witness_data.clone() {
+                    None => None,
+                    Some(witness) => {
+                        let res = execute_script_with_inputs(
+                            script! {
+                                {blake3_var_length(self.0.witness_size())}
+                            },
+                            witness,
+                        );
+                        let hash = witness_to_array(extract_witness_from_stack(res));
+                        Some(hash)
+                    }
+                }
+            }
+
+            fn to_hash_witness(&self) -> Option<Witness> {
+                match self.0.witness_data.clone() {
+                    None => None,
+                    Some(witness) => {
+                        let res = execute_script_with_inputs(
+                            script! {
+                                {blake3_var_length(self.0.witness_size())}
+                            },
+                            witness,
+                        );
+                        let witness = extract_witness_from_stack(res);
+                        Some(witness)
+                    }
+                }
+            }
+
+            fn size(&self) -> usize {
+                self.0.size
+            }
+
+            fn witness_size(&self) -> usize {
+                self.0.witness_size()
+            }
+
+            fn id(&self) -> &str {
+                &self.0.identity
+            }
+        }
+    };
 }
 
-/// impl element for Fq6
-impl ElementTrait for Fq6Type {
-    fn fill_with_data(&mut self, x: DataType) {
-        // TODO: need to be optimized and verify
-
-        match x {
-            DataType::Fq6Data(fq6_data) => {
-                let res = execute_script(script! {
-                    {fq6_push_not_montgomery(fq6_data)}
-                });
-                let witness = extract_witness_from_stack(res);
-                assert_eq!(witness.len(), self.0.witness_size());
-
-                self.0.witness_data = Some(witness);
-            }
-            _ => panic!("fill wrong data {:?}", x.type_id()),
-        }
-    }
-
-    fn to_witness(&self) -> Option<Witness> {
-        self.0.witness_data.clone()
-    }
-
-    fn from_witness(&self) -> Option<DataType> {
-        // TODO:
-        todo!()
-    }
-
-    fn to_hash(&self) -> Option<BLAKE3HASH> {
-        // TODO: need to be optimized and verify
-        match self.0.witness_data.clone() {
-            None => None,
-            Some(witness) => {
-                let res = execute_script_with_inputs(
-                    script! {
-                        {blake3_var_length(self.0.witness_size())}
-                    },
-                    witness,
-                );
-                let hash = witness_to_array(extract_witness_from_stack(res));
-                Some(hash)
-            }
-        }
-    }
-
-    fn to_hash_witness(&self) -> Option<Witness> {
-        match self.0.witness_data.clone() {
-            None => None,
-            Some(witness) => {
-                let res = execute_script_with_inputs(
-                    script! {
-                        {blake3_var_length(self.0.witness_size())}
-                    },
-                    witness,
-                );
-                let witness = extract_witness_from_stack(res);
-                Some(witness)
-            }
-        }
-    }
-
-    fn size(&self) -> usize {
-        self.0.size
-    }
-
-    fn witness_size(&self) -> usize {
-        self.0.witness_size()
-    }
-
-    fn id(&self) -> &str {
-        &self.0.identity
-    }
-}
-
-// TODO: impl element for G1point, G2point using macro!...
+// (Fq6)
+impl_element_trait!(Fq6Type, Fq6Data, 6, fq6_push_not_montgomery);
+// (Fq12)
+impl_element_trait!(Fq12Type, Fq12Data, 12, fq12_push_not_montgomery);
+// (x: Fq, y: Fq)
+impl_element_trait!(G1PointType, G1PointData, 2, G1Affine::push);
