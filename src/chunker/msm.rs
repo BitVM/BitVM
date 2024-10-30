fd914b5ca96136dd6f8b322e107b5a6ad51262ef
dongliangCai <22110240060@m.fudan.edu.cn>
2024-10-30 11:37:27 +0800

    add segment script len test

--- src/chunker/msm.rs
+++ src/chunker/msm.rs
@@ -20,11 +20,9 @@ fn hinted_scalar_mul_by_constant_g1_affine<T: BCAssigner>(
     p: &mut ark_bn254::G1Affine,
     coeff: Vec<(ark_bn254::Fq, ark_bn254::Fq)>,
     step_p: Vec<ark_bn254::G1Affine>,
-    trace: Vec<ark_bn254::G1Affine>,
 ) -> (Vec<Segment>, G1PointType) {
     let mut coeff_iter = coeff.iter();
     let mut step_p_iter = step_p.iter();
-    let mut trace_iter = trace.iter();
     let mut i = 0;
     // options: i_step = 2-15
     let i_step = 12;
@@ -36,21 +34,17 @@ fn hinted_scalar_mul_by_constant_g1_affine<T: BCAssigner>(
         p_mul.push((p_mul.last().unwrap().clone() + p.clone()).into_affine());
     }
 
-    // prepare for segment
-    let mut double_c_temp = vec![];
-    let mut step_temp = vec![];
-    let mut double_coeff_temp = vec![];
-    let mut add_c_temp = vec![];
-    let mut p_mul_mask_temp = vec![];
-    let mut add_coeff_temp = vec![];
-    let mut double_loop_script_temp = vec![];
-    let mut double_loop_hints_temp = vec![];
-    let mut add_loop_script_temp = vec![];
-    let mut add_loop_hints_temp = vec![];
+    // prepare intermediate state and hint for segment
+    let mut step_state = vec![];
+    let mut double_loop_script_state = vec![];
+    let mut double_loop_hints_state = vec![];
+    let mut add_loop_script_state = vec![];
+    let mut add_loop_hints_state = vec![];
     let mut intermediate_result = vec![];
     let mut dfs_script_0 = script!();
     
     let mut c: ark_bn254::G1Affine = ark_bn254::G1Affine::zero();
+    let mut step_count = 0;
 
     let scalar_bigint = scalar.into_bigint();
     while i < Fr::N_BITS {
@@ -59,34 +53,14 @@ fn hinted_scalar_mul_by_constant_g1_affine<T: BCAssigner>(
         if i > 0 {
             let double_coeff = coeff_iter.next().unwrap();
             let step = step_p_iter.next().unwrap();
-            
-            double_c_temp.push(c);
-            step_temp.push(*step);
-            double_coeff_temp.push(double_coeff);
+            step_state.push(*step);
 
             let (double_loop_script, doulbe_hints) = G1Affine::hinted_check_add(c, *step, double_coeff.0, double_coeff.1);
-            double_loop_script_temp.push(double_loop_script);
-            double_loop_hints_temp.push(doulbe_hints);
-
-            // let double_loop = script! {
-            //     // query bucket point through lookup table
-            //     { G1Affine::push_not_montgomery(*step) }
-            //     // check before usage
-            //     { double_loop_script }
-            // };
+            double_loop_script_state.push(double_loop_script);
+            double_loop_hints_state.push(doulbe_hints);
 
             c = (c + *step).into_affine();
         } 
-        // if i == i_step * 2 {
-        //     break;
-        // }
-
-        // squeeze a bucket scalar
-        // loop_scripts.push(script! {
-        //     for _ in 0..depth {
-        //         OP_FROMALTSTACK
-        //     }
-        // });
 
         let mut mask = 0;
 
@@ -101,12 +75,6 @@ fn hinted_scalar_mul_by_constant_g1_affine<T: BCAssigner>(
         } else {
             (ark_bn254::Fq::ZERO, ark_bn254::Fq::ZERO)
         };
-
-        if i > 0 {
-            add_c_temp.push(c);
-            p_mul_mask_temp.push(p_mul[mask as usize]);
-            add_coeff_temp.push(add_coeff);
-        }
 
         let (add_script, add_hints) = G1Affine::hinted_check_add(c, p_mul[mask as usize], add_coeff.0, add_coeff.1);
         
@@ -130,43 +98,46 @@ fn hinted_scalar_mul_by_constant_g1_affine<T: BCAssigner>(
                 { add_script }
             
             };
-            add_loop_script_temp.push(add_loop);
-            add_loop_hints_temp.push(add_hints);
+            add_loop_script_state.push(add_loop);
+            add_loop_hints_state.push(add_hints);
         }
 
         if mask != 0  {
             c = (c + p_mul[mask as usize]).into_affine();
         }
 
-        if i % 2 != 0 {
+        if step_count % 2 != 0 {
             intermediate_result.push(c);
         }
 
+        step_count += 1;
         i += i_step;
     }
     assert!(coeff_iter.next() == None);
     assert!(step_p_iter.next() == None);
-    assert!(trace_iter.next() == None);
     *p = c;
 
     let mut segment = vec![];
+
+    //construct segment0
 
     let segment_script0 = script! {
         // query bucket point through lookup table
         { Fr::convert_to_le_bits_toaltstack() }
         { dfs_script_0 }
-        {double_loop_script_temp[0].clone()}
-        {add_loop_script_temp[0].clone()}  
+        {double_loop_script_state[0].clone()}
+        {add_loop_script_state[0].clone()}  
     };
+    println!("script len:{:?}", segment_script0.len());
 
     let mut segment_hint0 = vec![];
 
-    segment_hint0.append(&mut double_loop_hints_temp[0]);
-    segment_hint0.append(&mut add_loop_hints_temp[0]);
+    segment_hint0.append(&mut double_loop_hints_state[0]);
+    segment_hint0.append(&mut add_loop_hints_state[0]);
 
 
     let mut step0 = G1PointType::new(assigner, "step0");
-    step0.fill_with_data(G1PointData(step_temp[0]));
+    step0.fill_with_data(G1PointData(step_state[0]));
     let mut result0 = G1PointType::new(assigner, "result0");
     result0.fill_with_data(G1PointData(intermediate_result[0]));
 
@@ -180,25 +151,29 @@ fn hinted_scalar_mul_by_constant_g1_affine<T: BCAssigner>(
     let mut prev_result = result0;
     let mut i = 1;
 
-    //two double_loop + add_loop as a sengment
+    //construct other segment
+    //two (double_loop + add_loop) as a sengment
+
     while i < 20 {
+
         let segment_script = script! {
-            {double_loop_script_temp[i].clone()}
-            {add_loop_script_temp[i].clone()}
-            {double_loop_script_temp[i+1].clone()}
-            {add_loop_script_temp[i+1].clone()}    
+            {double_loop_script_state[i].clone()}
+            {add_loop_script_state[i].clone()}
+            {double_loop_script_state[i+1].clone()}
+            {add_loop_script_state[i+1].clone()}    
         };
+        println!("script len:{:?}", segment_script.len());
 
         let mut segment_hint = vec![];
-        segment_hint.append(&mut double_loop_hints_temp[i]);
-        segment_hint.append(&mut add_loop_hints_temp[i]);
-        segment_hint.append(&mut double_loop_hints_temp[i+1]);
-        segment_hint.append(&mut add_loop_hints_temp[i+1]);
+        segment_hint.append(&mut double_loop_hints_state[i]);
+        segment_hint.append(&mut add_loop_hints_state[i]);
+        segment_hint.append(&mut double_loop_hints_state[i+1]);
+        segment_hint.append(&mut add_loop_hints_state[i+1]);
 
         let mut step1 = G1PointType::new(assigner, &format!("step{}", i));
-        step1.fill_with_data(G1PointData(step_temp[i]));
+        step1.fill_with_data(G1PointData(step_state[i]));
         let mut step2 = G1PointType::new(assigner, &format!("step{}", i + 1));
-        step2.fill_with_data(G1PointData(step_temp[i+1]));
+        step2.fill_with_data(G1PointData(step_state[i+1]));
 
         let mut result = G1PointType::new(assigner, &format!("result{}", (i+1)/2));
         result.fill_with_data(G1PointData(intermediate_result[(i+1)/2]));
@@ -213,8 +188,45 @@ fn hinted_scalar_mul_by_constant_g1_affine<T: BCAssigner>(
         segment.push(segment_i);
 
         prev_result = result;
+
         i+=2;
     }
 
     (segment, prev_result)
+}
+
+#[cfg(test)]
+mod test {
+    use ark_bn254::{Fr, G1Affine};
+    use ark_ec::CurveGroup;
+    use ark_ff::UniformRand;
+    use ark_std::test_rng;
+
+    use crate::{bn254::msm::prepare_msm_input, chunker::assigner::DummyAssinger};
+
+    use super::hinted_scalar_mul_by_constant_g1_affine;
+
+    #[test]
+    fn test_msm_script_len() {
+        let mut assigner = DummyAssinger {};
+        let k = 0;
+        let n = 1 << k;
+        let rng = &mut test_rng();
+
+        let scalars = (0..n).map(|_| ark_bn254::Fr::rand(rng)).collect::<Vec<_>>();
+
+        let mut bases = (0..n)
+            .map(|_| ark_bn254::G1Projective::rand(rng).into_affine())
+            .collect::<Vec<_>>();
+        let (inner_coeffs, _) = prepare_msm_input(&bases, &scalars, 12);
+
+        let _ = hinted_scalar_mul_by_constant_g1_affine(    
+            &mut assigner,    
+            "no_prefix",    
+            scalars[0],
+            &mut bases[0],
+            inner_coeffs[0].0.clone(),
+            inner_coeffs[0].1.clone(),
+        );
+    }
 }
