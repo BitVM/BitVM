f650b57841fd30f6421d5f4e6adfb85e8f46c9e9
Ekrem BAL <mail.ekrembal@gmail.com>
2024-12-19 15:55:26 +0300

    Convert to workspace

---
+++ bitvm/src/chunker/common.rs
@@ -0,0 +1,109 @@
+use bitcoin::script::write_scriptint;
+
+use crate::{treepp::*, ExecuteInfo};
+
+/// Define Witness
+pub type RawWitness = Vec<Vec<u8>>;
+
+/// Should use u32 version's blake3 hash for fq element
+pub use crate::hash::blake3_u32::blake3_var_length;
+
+/// The depth of a blake3 hash, depending on the defination of `N_DIGEST_U32_LIMBS`
+pub const BLAKE3_HASH_LENGTH: usize =
+    crate::hash::blake3_u32::N_DIGEST_U32_LIMBS as usize * 4;
+pub type BLAKE3HASH = [u8; BLAKE3_HASH_LENGTH];
+
+/// Return witness size of bytes.
+pub fn witness_size(witness: &RawWitness) -> usize {
+    let mut sum = 0;
+    for x in witness {
+        sum += x.len();
+    }
+    sum
+}
+
+/// 1 means not equal, 0 means equal.
+/// If n is non 0, compare two element of n length is equal or not and left 0 or 1 on stack.
+/// If n is 0, return 0.
+pub fn not_equal(n: usize) -> Script {
+    if n == 0 {
+        return script! {OP_FALSE};
+    }
+
+    script!(
+        for i in 0..n {
+            {i + n}
+            OP_PICK
+            {i + 1}
+            OP_PICK
+            OP_EQUAL
+            OP_TOALTSTACK
+        }
+
+        for _ in 0..2*n {
+            OP_DROP
+        }
+
+        OP_FROMALTSTACK
+
+        for _ in 0..n-1 {
+            OP_FROMALTSTACK
+            OP_BOOLAND
+        }
+
+        OP_NOT
+    )
+}
+
+/// From witness to hash
+pub fn witness_to_array(witness: RawWitness) -> BLAKE3HASH {
+    assert_eq!(witness.len(), BLAKE3_HASH_LENGTH);
+    let mut res: BLAKE3HASH = [0; BLAKE3_HASH_LENGTH];
+    for (idx, byte) in witness.iter().enumerate() {
+        if byte.is_empty() {
+            res[idx] = 0;
+        } else {
+            res[idx] = byte[0];
+        }
+    }
+    res
+}
+
+/// From hash to witness
+pub fn array_to_witness(hash: BLAKE3HASH) -> RawWitness {
+    let mut witness = vec![];
+    for byte in hash {
+        let mut out: [u8; 8] = [0; 8];
+        let length = write_scriptint(&mut out, byte as i64);
+        witness.push(out[0..length].to_vec());
+    }
+    witness
+}
+
+/// Extract witness from stack.
+pub fn extract_witness_from_stack(res: ExecuteInfo) -> RawWitness {
+    res.final_stack.0.iter_str().fold(vec![], |mut vector, x| {
+        vector.push(x);
+        vector
+    })
+}
+
+/// Compare two elements of n length.
+/// If them are not equal, return script's failure directly.
+pub fn equalverify(n: usize) -> Script {
+    script!(
+        for _ in 0..n {
+            OP_TOALTSTACK
+        }
+
+        for i in 1..n {
+            {i}
+            OP_ROLL
+        }
+
+        for _ in 0..n {
+            OP_FROMALTSTACK
+            OP_EQUALVERIFY
+        }
+    )
+}
---
+++ bridge/tests/bridge/serialization/peg_out_graph.rs
@@ -0,0 +1,67 @@
+use bitcoin::{Address, Amount};
+
+use bridge::{
+    graphs::{
+        base::{FEE_AMOUNT, INITIAL_AMOUNT},
+        peg_in::PegInGraph,
+        peg_out::PegOutGraph,
+    },
+    scripts::generate_pay_to_pubkey_script_address,
+    serialization::{deserialize, serialize},
+    transactions::base::Input,
+};
+
+use crate::bridge::faucet::{Faucet, FaucetType};
+
+use super::super::{helper::generate_stub_outpoint, setup::setup_test};
+
+#[tokio::test]
+async fn test_peg_out_graph_serialization() {
+    let config = setup_test().await;
+    let faucet = Faucet::new(FaucetType::EsploraRegtest);
+
+    let mut funding_inputs: Vec<(&Address, Amount)> = vec![];
+    let amount = Amount::from_sat(INITIAL_AMOUNT + FEE_AMOUNT);
+    let address = generate_pay_to_pubkey_script_address(
+        config.depositor_context.network,
+        &config.depositor_context.depositor_public_key,
+    );
+    funding_inputs.push((&address, amount));
+
+    let kick_off_amount = Amount::from_sat(INITIAL_AMOUNT + FEE_AMOUNT); // Arbitrary amount
+    let kick_off_address = generate_pay_to_pubkey_script_address(
+        config.operator_context.network,
+        &config.operator_context.operator_public_key,
+    );
+    funding_inputs.push((&kick_off_address, kick_off_amount));
+    faucet
+        .fund_inputs(&config.client_0, &funding_inputs)
+        .await
+        .wait()
+        .await;
+
+    let outpoint = generate_stub_outpoint(&config.client_0, &address, amount).await;
+
+    let peg_in_graph = PegInGraph::new(
+        &config.depositor_context,
+        Input { outpoint, amount },
+        &config.depositor_evm_address,
+    );
+
+    let kick_off_outpoint =
+        generate_stub_outpoint(&config.client_0, &kick_off_address, kick_off_amount).await;
+
+    let (peg_out_graph, _) = PegOutGraph::new(
+        &config.operator_context,
+        &peg_in_graph,
+        Input {
+            outpoint: kick_off_outpoint,
+            amount: kick_off_amount,
+        },
+    );
+
+    let json = serialize(&peg_out_graph);
+    assert!(!json.is_empty());
+    let deserialized_peg_out_graph = deserialize::<PegOutGraph>(&json);
+    assert!(peg_out_graph == deserialized_peg_out_graph);
+}
