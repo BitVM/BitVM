4e54bf25a47b2b96664b811215daa66832252208
Lukas George <48297887+lucidLuckylee@users.noreply.github.com>
2025-01-23 18:03:05 +0100

    Revert "Add header chain circuit and its host with ELFs (#178)" (#203)
    This reverts commit b435ff660bc90b1b916c4c3ad853e28b12cefb27.

--- header-chain/header-chain-guest/guest/src/main.rs
+++
@@ -1,6 +0,0 @@
-#![no_main]
-header_chain::risc0_zkvm::guest::entry!(main);
-fn main() {
-    let zkvm_guest = header_chain::zkvm::Risc0Guest::new();
-    header_chain::header_chain_circuit(&zkvm_guest);
-}
