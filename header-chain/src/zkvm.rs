844e4053a9abed569854725fdb2ad21a44caed03
ozankaymak <ozan.kaymak@boun.edu.tr>
2025-03-04 12:58:01 +0300

    refactor: streamline code formatting and update binary data files for various networks

--- header-chain/src/zkvm.rs
+++ header-chain/src/zkvm.rs
@@ -29,9 +29,7 @@ pub trait ZkvmHost {
 pub struct Risc0Guest;
 
 impl Risc0Guest {
-    pub fn new() -> Self {
-        Self {}
-    }
+    pub fn new() -> Self { Self {} }
 }
 
 impl ZkvmGuest for Risc0Guest {
