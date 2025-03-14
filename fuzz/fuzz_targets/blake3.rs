668c051eb5a9894fa6440d4c31640df9a682281d
Christian Lewe <ch_lewe@agdsn.me>
2025-03-14 14:58:35 +0100

    refactor: Blake3 fuzz target
    Cover all message sizes that are used inside BitVM.

--- fuzz/fuzz_targets/blake3.rs
+++ fuzz/fuzz_targets/blake3.rs
@@ -1,28 +1,75 @@
 #![no_main]
 
+use std::iter::IntoIterator;
 use std::sync::LazyLock;
 
+use arbitrary::Arbitrary;
 use bitcoin::ScriptBuf;
 use bitcoin_script_stack::optimizer;
 use libfuzzer_sys::fuzz_target;
-
 use bitvm::execute_script_buf;
 use bitvm::hash::blake3::{
     blake3_compute_script, blake3_push_message_script, blake3_verify_output_script,
 };
 
-static BLAKE3_COMPUTE_SCRIPT: LazyLock<ScriptBuf> =
-    LazyLock::new(|| optimizer::optimize(blake3_compute_script(32).compile()));
+static BLAKE3_COMPUTE_SCRIPTS: LazyLock<[ScriptBuf; 4]> =
+    LazyLock::new(|| [64, 128, 192, 448]
+            .into_iter()
+            .map(|msg_len| blake3_compute_script(msg_len).compile())
+            .map(|script| optimizer::optimize(script))
+            .collect::<Vec<ScriptBuf>>()
+            .try_into().unwrap()
+    );
 
-fuzz_target!(|message: [u8; 32]| {
-    let expected_hash = blake3::hash(&message).as_bytes().clone();
+/// Cover all message sizes that are used inside BitVM.
+///
+/// Each message size is processed by a different BLAKE3 Bitcoin script.
+/// Computing and optimizing each of these scripts takes considerable time.
+/// Even when we cache the result, the fuzzer has to wait until all scripts are gennerated.
+///
+/// In order to get useful coverage while keeping computational effort low,
+/// we fuzz exactly the message sizes that are used inside BitVM.
+/// It turns out, there are only four different sizes.
+#[derive(Debug, Clone, Arbitrary)]
+enum MessageBytes {
+    U64([u8; 64]),
+    U128([u8; 128]),
+    U192([u8; 192]),
+    U448([u8; 448]),
+}
 
-    let mut bytes = blake3_push_message_script(&message).compile().to_bytes();
-    bytes.extend_from_slice(BLAKE3_COMPUTE_SCRIPT.as_bytes());
-    bytes.extend(
+impl AsRef<[u8]> for MessageBytes {
+    fn as_ref(&self) -> &[u8] {
+        match self {
+            Self::U64(bytes) => bytes,
+            Self::U128(bytes) => bytes,
+            Self::U192(bytes) => bytes,
+            Self::U448(bytes) => bytes,
+        }
+    }
+}
+
+impl MessageBytes {
+    pub fn blake3_compute_script(&self) -> &'static ScriptBuf {
+        let index = match self {
+            Self::U64(..) => 0,
+            Self::U128(..) => 1,
+            Self::U192(..) => 2,
+            Self::U448(..) => 3,
+        };
+        &BLAKE3_COMPUTE_SCRIPTS[index]
+    }
+}
+
+fuzz_target!(|message: MessageBytes| {
+    let expected_hash = blake3::hash(message.as_ref()).as_bytes().clone();
+
+    let mut bytes = blake3_push_message_script(message.as_ref()).compile().to_bytes();
+    bytes.extend_from_slice(message.blake3_compute_script().as_bytes());
+    bytes.extend_from_slice(
         blake3_verify_output_script(expected_hash)
             .compile()
-            .to_bytes(),
+            .as_bytes(),
     );
     let script = ScriptBuf::from_bytes(bytes);
     assert!(execute_script_buf(script).success);
