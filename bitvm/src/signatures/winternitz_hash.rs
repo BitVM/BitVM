82a44f895ab366a2ed4201983800904419016c48
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-04-18 11:22:04 +0200

    Update winternitz_hash.rs

--- bitvm/src/signatures/winternitz_hash.rs
+++ bitvm/src/signatures/winternitz_hash.rs
@@ -15,11 +15,11 @@ pub static WINTERNITZ_HASH_VERIFIER: Winternitz<ListpickVerifier, ToBytesConvert
 pub static WINTERNITZ_MESSAGE_VERIFIER: Winternitz<ListpickVerifier, VoidConverter> =
     Winternitz::new();
 
-/// Winternitz verifier, returns the message in in bytes
+/// Winternitz verifier, returns the message in bytes
 pub static WINTERNITZ_VARIABLE_VERIFIER: Winternitz<ListpickVerifier, ToBytesConverter> =
     Winternitz::new();
 
-/// Winternitz verifier for compact signature representation, returns the message in in bytes
+/// Winternitz verifier for compact signature representation, returns the message in bytes
 pub static WINTERNITZ_MESSAGE_COMPACT_VERIFIER: Winternitz<BruteforceVerifier, VoidConverter> =
     Winternitz::new();
 
