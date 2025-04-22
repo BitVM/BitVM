eb9f6251a61575ba0ff673bc741614fa81599297
danielntmd <162406516+danielntmd@users.noreply.github.com>
2025-04-22 06:00:33 -0400

    test: bigint limb transforms fuzz (#274)
    * WIP: Bigint configuration
    
    * feat: bigint transforms fuzzing
    
    * test: bigint limb transforms fuzzing
    -add: macro for cleaning, adjusted test parameter, and idiomatic cleanup
    
    * clean: separate files and remove unncessary struct param
    
    * chore: cargo fmt
    
    * Revert "chore: cargo fmt"
    
    This reverts commit 0ea384abaeb24e67b311412173bd2e4377fcd5f9.
    
    * chore: cargo formatting error, remove single line enforcement
    
    * chore: move stack element verify script to fuzz library

---
+++ fuzz/fuzz_targets/bigint/macros.rs
@@ -0,0 +1,11 @@
+#[macro_export]
+macro_rules! match_bigint_type {
+    ($bigint_enum:expr, $method:ident $(, $args:expr)* ) => {
+        match $bigint_enum {
+            BigIntType::U64(_)  => U64::$method($($args),*),
+            BigIntType::U254(_) => U254::$method($($args),*),
+            BigIntType::U256(_) => U256::$method($($args),*),
+            BigIntType::U384(_) => U384::$method($($args),*),
+        }
+    };
+}
